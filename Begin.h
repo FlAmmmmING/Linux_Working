2e5a9c1c57ab9ec20ea8e35e0accef9ff780ea63
FlAmmmmING <110998609+FlAmmmmING@users.noreply.github.com>
2023-05-11 00:24:30 +0800

    Update Begin.h

--- Begin.h
+++ Begin.h
@@ -8,59 +8,62 @@ void Begin()
 {
     flash();
     int choice = 100;
-    User* U = new User; //开一个空间给可能需要登陆的玩家 
-    player* play = new player; // 游玩角色，需要用指针类型
-    while (choice) {
+    User *U = new User;        // 开一个空间给可能需要登陆的玩家
+    player *play = new player; // 游玩角色，需要用指针类型
+    while (choice)
+    {
         cin >> choice;
-        switch (choice){
-            case 1: 
-                playMinesweeper(play);
-                is_formal ? flash(U) : flash();
+        switch (choice)
+        {
+        case 1:
+            playMinesweeper(play);
+            is_formal ? flash(U) : flash();
             break;
-            case 2: 
-                playHuarong(play);
-                is_formal ? flash(U) : flash();
+        case 2:
+            playHuarong(play);
+            is_formal ? flash(U) : flash();
             break;
-            case 3:
-                playMaze(play);
-                is_formal ? flash(U) : flash();
+        case 3:
+            playMaze(play);
+            is_formal ? flash(U) : flash();
             break;
-            case 8:
-                if(is_formal == 0) // 我目前没有登陆
-                {
-                    U->FindUser();
-                    play->inputplayer(U); // 导入
-                }
-                else {
-                    is_formal = 0; // 否则注销
-                    cout << "Log out! Your score has been saved!" << endl;
-                    U->exchangescore(play->return_score()); // 获得当前分数，将分数覆盖到文档User
-                    play->clearscore();
-                    sleep(3);
-                }
-                is_formal ? flash(U) : flash();
-            break;
-            case 9:
-                play->show_score();
+        case 8:
+            if (is_formal == 0) // 我目前没有登陆
+            {
+                U->FindUser();
+                play->inputplayer(U); // 导入
+            }
+            else
+            {
+                is_formal = 0; // 否则注销
+                cout << "Log out! Your score has been saved!" << endl;
+                U->exchangescore(play->return_score()); // 获得当前分数，将分数覆盖到文档User
+                play->clearscore();
                 sleep(3);
-                system("clear");
-                is_formal ? flash(U) : flash();
+            }
+            is_formal ? flash(U) : flash();
             break;
-            case 0:
-                if (is_formal) {
-                    cout << "Log out! Your score has been saved!" << endl;
-                    U->exchangescore(play->return_score()); // 获得当前分数，将分数覆盖到文档User
-                    play->clearscore();
-                }
-                cout << "Exit!" << endl;
-                sleep(3);
-                system("clear");
+        case 9:
+            play->show_score();
+            sleep(3);
+            system("clear");
+            is_formal ? flash(U) : flash();
             break;
-            default:
-                cout << "Invaild Input! Please input again!" << endl;
-                choice = 1;
+        case 0:
+            if (is_formal)
+            {
+                cout << "Log out! Your score has been saved!" << endl;
+                U->exchangescore(play->return_score()); // 获得当前分数，将分数覆盖到文档User
+                play->clearscore();
+            }
+            cout << "Exit!" << endl;
+            sleep(3);
+            system("clear");
+            break;
+        default:
+            cout << "Invaild Input! Please input again!" << endl;
+            choice = 1;
             break;
         }
-        
     }
 }
