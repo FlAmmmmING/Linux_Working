d24ff49ba33903b0cf793a4409fef34f7e9ae3ee
FlAmmmmING <110998609+FlAmmmmING@users.noreply.github.com>
2023-05-10 22:47:51 +0800

    Update Begin.h

--- Begin.h
+++ Begin.h
@@ -8,27 +8,42 @@ void Begin()
 {
     flash();
     int choice = 100;
+    User* U = new User; //开一个空间给可能需要登陆的玩家 
     player* play = new player; // 游玩角色，需要用指针类型
     while (choice) {
         cin >> choice;
         switch (choice){
             case 1: 
                 playMinesweeper(play);
-                flash();
+                is_formal ? flash(U) : flash();
             break;
             case 2: 
                 playHuarong(play);
-                flash();
+                is_formal ? flash(U) : flash();
             break;
             case 3:
                 playMaze(play);
-                flash();
+                is_formal ? flash(U) : flash();
+            break;
+            case 8:
+                if(is_formal == 0) // 我目前没有登陆
+                {
+                    U->FindUser();
+                    play->inputplayer(U); // 导入
+                }
+                else {
+                    is_formal = 0; // 否则注销
+                    cout << "Log out!" << endl;
+                    play->clearscore();
+                    sleep(3);
+                }
+                is_formal ? flash(U) : flash();
             break;
             case 9:
                 play->show_score();
                 sleep(3);
                 system("clear");
-                menu();
+                is_formal ? flash(U) : flash();
             break;
             case 0:
                 cout << "Exit!" << endl;
