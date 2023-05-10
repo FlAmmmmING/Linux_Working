f89aa4da08d455a9c6727204175529d334b89777
FlAmmmmING <110998609+FlAmmmmING@users.noreply.github.com>
2023-05-11 00:22:53 +0800

    Update Huarong.h

--- Huarong.h
+++ Huarong.h
@@ -8,12 +8,12 @@ class Huarong
 private:
 	int order; // 阶数
 	int *arr;
-	int cnt = 0; // 用于记录走了多少步
+	int cnt = 0;  // 用于记录走了多少步
 	bool jud = 0; // 用于判断用户是否选择中途退出游戏
-	player* p; // 当前游戏玩家
+	player *p;	  // 当前游戏玩家
 
 public:
-	Huarong(int n, player* player) // 构造函数中传进游玩的玩家信息
+	Huarong(int n, player *player) // 构造函数中传进游玩的玩家信息
 	{
 		order = n;
 		arr = new int[order * order];
@@ -52,6 +52,11 @@ public:
 				cout << endl;
 			}
 		}
+		// 打印操作盘
+		cout << endl;
+		cout << "    W" << endl;
+		cout << "  A   D  " << endl;
+		cout << "    S" << endl;
 	}
 	bool Finish()
 	{
@@ -67,76 +72,76 @@ public:
 	void play()
 	{
 		print();
-		while (Finish() != true && !jud)
+		if (jud)
 		{
-			move();
-			print();
-		}
-		if (jud) {
 			cout << "exit game!" << endl;
 			cout << "You lose " << 10 * (order - 1) << " scores!" << endl;
 			p->lose_score(10 * (order - 1));
 		}
-		else {
+		else if (Finish())
+		{
 			cout << "You Win!" << endl;
 			cout << "You get " << max(10 * (order - 1) - cnt, 10 * (order - 2)) << " scores!" << endl;
 			p->get_score(max(10 * (order - 1) - cnt, 10 * (order - 2)));
 		}
-		sleep(3);
 	}
-	void move()
+	bool Exit()
 	{
-		char a;
-		do
+		return jud;
+	}
+	void move(char a)
+	{
+		if (a == 'q')
 		{
-			cout << "please move : ↑(w)↓(s)←(a)→(d):>";
-			cin >> a;
-			if (a == 'q')
+			cout << "Are you shure? It will deduct your secore!(Y/N)" << endl;
+			char jud2;
+			cin >> jud2; // 再次确认是否要中途退出
+			if (jud2 == 'Y')
 			{
-				cout << "Are you shure? It will deduct your secore!(Y/N)" << endl;
-				char jud2;
-				cin >> jud2; // 再次确认是否要中途退出
-				if (jud2 == 'Y') {
-					jud = 1;
-					cnt = max(0, cnt - 1);
-					return;
-				}
-				else {
-					system("clear");
-					cnt = max(0, cnt - 1);
-					print();
-					continue;
-				}
+				jud = 1;
+				cnt = max(0, cnt - 1);
+				return;
 			}
-		} while (!((a == 's' && (FindBlank() - order) >= 0) || (a == 'w' && (FindBlank() + order) < order * order) || (a == 'd' && (FindBlank() - 1) >= 0 && FindBlank() % order != 0) || (a == 'a' && (FindBlank() + 1) < order * order && (FindBlank() + 1) % order != 0)));
+			else
+			{
+				system("clear");
+				cnt = max(0, cnt - 1);
+				print();
+			}
+		}
 		cnt++;
-		if (a == 'w')
+		if (a == 'w' && (FindBlank() + order) < order * order)
 		{
 			int pos = FindBlank();
 			int x = arr[pos];
 			arr[pos] = arr[pos + order];
 			arr[pos + order] = x;
 		}
-		else if (a == 's')
+		else if (a == 's' && (FindBlank() - order) >= 0)
 		{
 			int pos = FindBlank();
 			int x = arr[pos];
 			arr[pos] = arr[pos - order];
 			arr[pos - order] = x;
 		}
-		else if (a == 'a')
+		else if (a == 'a' && (FindBlank() + 1) < order * order && (FindBlank() + 1) % order != 0)
 		{
 			int pos = FindBlank();
 			int x = arr[pos];
 			arr[pos] = arr[pos + 1];
 			arr[pos + 1] = x;
 		}
-		else if (a == 'd')
+		else if (a == 'd' && (FindBlank() - 1) >= 0 && FindBlank() % order != 0)
 		{
 			int pos = FindBlank();
 			int x = arr[pos];
 			arr[pos] = arr[pos - 1];
 			arr[pos - 1] = x;
+		}
+		else
+		{
+			move(getch());
+			cnt = max(0, cnt - 1);
 		}
 	}
 	int FindBlank() // 判断哪里是空的
@@ -152,18 +157,29 @@ public:
 	}
 };
 
-void playHuarong(player* play) 
+void playHuarong(player *play)
 {
 	system("clear");
 
 	cout << "Please input the order of Huarong Road(2 ~ 10):>";
-    int order; cin >> order;
-	while (order < 2 || order > 10) 
+	int order;
+	cin >> order;
+	while (order < 2 || order > 10)
 	{
 		cout << "Invalid input! The length of order should be in range of 2 to 10" << endl;
 		cout << "Please input again:>";
-		cin >> order;	
+		cin >> order;
 	}
-    Huarong hr(order, play);
-    hr.play();
+	Huarong hr(order, play);
+	while (true)
+	{
+		hr.play();
+		hr.move(getch());
+		if (hr.Exit() || hr.Finish())
+		{
+			hr.play();
+			break;
+		}
+	}
+	sleep(3);
 }
